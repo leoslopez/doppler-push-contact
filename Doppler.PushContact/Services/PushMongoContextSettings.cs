9005707e72c5afb9fe5dafd928aa80142b2750e4
Emanuel <ecacheda@makingsense.com>
2022-07-01 17:29:29 -0700

    feat: allow extra parameters in connection string

--- Doppler.PushContact/Services/PushMongoContextSettings.cs
+++ Doppler.PushContact/Services/PushMongoContextSettings.cs
@@ -2,9 +2,7 @@ namespace Doppler.PushContact.Services
 {
     public class PushMongoContextSettings
     {
-        public string Host { get; set; }
-
-        public string Username { get; set; }
+        public string ConnectionString { get; set; }
 
         public string Password { get; set; }
 
