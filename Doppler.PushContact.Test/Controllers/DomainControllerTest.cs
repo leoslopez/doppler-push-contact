2c38d0d25cd254828ad56ca04f4771da0c36be94
leoslopez <llopez@makingsense.com>
2022-10-11 15:56:42 -0300

    test: add unit tests to the new method
    add unit tests to the "CreateMessageAssociatedToDomain" method
    
    related to DOP-1171

--- Doppler.PushContact.Test/Controllers/DomainControllerTest.cs
+++ Doppler.PushContact.Test/Controllers/DomainControllerTest.cs
@@ -1,6 +1,7 @@
 using AutoFixture;
 using Doppler.PushContact.Models;
 using Doppler.PushContact.Services;
+using Doppler.PushContact.Services.Messages;
 using Doppler.PushContact.Test.Controllers.Utils;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -326,6 +327,157 @@ namespace Doppler.PushContact.Test.Controllers
 
             // Assert
             Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(TestApiUsersData.TOKEN_EMPTY)]
+        [InlineData(TestApiUsersData.TOKEN_BROKEN)]
+        public async Task CreateMessageAssociatedToDomain_should_return_unauthorized_when_token_is_not_valid(string token)
+        {
+            // Arrange
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions());
+
+            var fixture = new Fixture();
+            var name = fixture.Create<string>();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"domains/{name}/message")
+            {
+                Headers = { { "Authorization", $"Bearer {token}" } }
+            };
+
+            // Act
+            var response = await client.SendAsync(request);
+            _output.WriteLine(response.GetHeadersAsString());
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(TestApiUsersData.TOKEN_SUPERUSER_EXPIRE_20010908)]
+        public async Task CreateMessageAssociatedToDomain_should_return_unauthorized_when_token_is_a_expired_superuser_token(string token)
+        {
+            // Arrange
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions());
+
+            var fixture = new Fixture();
+            var name = fixture.Create<string>();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"domains/{name}/message")
+            {
+                Headers = { { "Authorization", $"Bearer {token}" } }
+            };
+
+            // Act
+            var response = await client.SendAsync(request);
+            _output.WriteLine(response.GetHeadersAsString());
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(TestApiUsersData.TOKEN_EXPIRE_20330518)]
+        [InlineData(TestApiUsersData.TOKEN_SUPERUSER_FALSE_EXPIRE_20330518)]
+        [InlineData(TestApiUsersData.TOKEN_ACCOUNT_123_TEST1_AT_TEST_DOT_COM_EXPIRE_20330518)]
+        public async Task CreateMessageAssociatedToDomain_should_require_a_valid_token_with_isSU_flag(string token)
+        {
+            // Arrange
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions());
+
+            var fixture = new Fixture();
+            var name = fixture.Create<string>();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"domains/{name}/message")
+            {
+                Headers = { { "Authorization", $"Bearer {token}" } }
+            };
+
+            // Act
+            var response = await client.SendAsync(request);
+            _output.WriteLine(response.GetHeadersAsString());
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateMessageAssociatedToDomain_should_return_unauthorized_when_authorization_header_is_empty()
+        {
+            // Arrange
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions());
+
+            var fixture = new Fixture();
+            var name = fixture.Create<string>();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"domains/{name}/message");
+
+            // Act
+            var response = await client.SendAsync(request);
+            _output.WriteLine(response.GetHeadersAsString());
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateMessageAssociatedToDomain_should_create_a_message_with_summarization_fields_equal_cero_and_return_proper_messageId()
+        {
+            // Arrange
+            var fixture = new Fixture();
+
+            var qSent = 0;
+            var qDelivery = 0;
+            var qNotDelivery = 0;
+
+            var domain = "aTestDomain";
+            var message = new Message
+            {
+                Title = fixture.Create<string>(),
+                Body = fixture.Create<string>(),
+                OnClickLink = fixture.Create<string>(),
+                ImageUrl = fixture.Create<string>()
+            };
+
+            var domainServiceMock = new Mock<IDomainService>();
+            var messageRepositoryMock = new Mock<IMessageRepository>();
+
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton(domainServiceMock.Object);
+                    services.AddSingleton(messageRepositoryMock.Object);
+                });
+
+            }).CreateClient(new WebApplicationFactoryClientOptions());
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"domains/{domain}/message")
+            {
+                Headers = { { "Authorization", $"Bearer {TestApiUsersData.TOKEN_SUPERUSER_EXPIRE_20330518}" } },
+                Content = JsonContent.Create(message)
+            };
+
+            // Act
+            var response = await client.SendAsync(request);
+            _output.WriteLine(response.GetHeadersAsString());
+
+            // Assert
+            messageRepositoryMock.Verify(mock => mock.AddAsync(
+                It.IsAny<Guid>(),
+                domain,
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                qSent,
+                qDelivery,
+                qNotDelivery,
+                It.IsAny<string>()
+            ), Times.Once());
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            var messageResult = await response.Content.ReadFromJsonAsync<MessageResult>();
+            Assert.IsType<Guid>(messageResult.MessageId);
         }
     }
 }
