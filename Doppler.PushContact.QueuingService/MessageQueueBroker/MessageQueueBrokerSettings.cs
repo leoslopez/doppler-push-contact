285dc2996deddae78cc21e733116b8d05c4cec46
leoslopez <llopez@makingsense.com>
2024-06-06 14:27:05 -0300

    fix: whitespace and invalid charset

--- Doppler.PushContact.QueuingService/MessageQueueBroker/MessageQueueBrokerSettings.cs
+++ Doppler.PushContact.QueuingService/MessageQueueBroker/MessageQueueBrokerSettings.cs
@@ -2,11 +2,10 @@ namespace Doppler.PushContact.QueuingService.MessageQueueBroker
 {
     public class MessageQueueBrokerSettings
     {
-
         public required string ConnectionString { get; set; }
 
         /// <summary>
-        /// Password for connect to MessageQueueSubscriber. 
+        /// Password for connect to MessageQueueSubscriber.
         /// If ConnectionString has defined password parameter, will be replaced with this value if it is not empty.
         /// </summary>
         public string? Password { get; set; }
