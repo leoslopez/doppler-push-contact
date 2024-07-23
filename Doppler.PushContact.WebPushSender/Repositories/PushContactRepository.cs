06a365b82a97e92ab4edd67d7258474975e54efe
leoslopez <llopez@makingsense.com>
2024-07-23 15:07:50 -0300

    chore: add logging for a database exception

--- Doppler.PushContact.WebPushSender/Repositories/PushContactRepository.cs
+++ Doppler.PushContact.WebPushSender/Repositories/PushContactRepository.cs
@@ -1,9 +1,11 @@
 using Doppler.PushContact.Models.Entities;
 using Doppler.PushContact.WebPushSender.Repositories.Interfaces;
 using Doppler.PushContact.WebPushSender.Repositories.Setup;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
 using System.Threading.Tasks;
 
 namespace Doppler.PushContact.WebPushSender.Repositories
@@ -11,10 +13,16 @@ namespace Doppler.PushContact.WebPushSender.Repositories
     public class PushContactRepository : IPushContactRepository
     {
         private readonly IMongoCollection<BsonDocument> _collection;
+        private readonly ILogger<PushContactRepository> _logger;
 
-        public PushContactRepository(IMongoDatabase database, IOptions<RepositorySettings> repositorySettings)
+        public PushContactRepository(
+            IMongoDatabase database,
+            IOptions<RepositorySettings> repositorySettings,
+            ILogger<PushContactRepository> logger
+        )
         {
             _collection = database.GetCollection<BsonDocument>(repositorySettings.Value.PushContactsCollectionName);
+            _logger = logger;
         }
 
         public async Task<bool> MarkDeletedAsync(string pushContactId)
@@ -23,9 +31,16 @@ namespace Doppler.PushContact.WebPushSender.Repositories
 
             var update = Builders<BsonDocument>.Update.Set(PushContactDocumentProps.DeletedPropName, true);
 
-            var result = await _collection.UpdateOneAsync(filter, update);
-
-            return result != null && result.ModifiedCount > 0;
+            try
+            {
+                var result = await _collection.UpdateOneAsync(filter, update);
+                return result != null && result.ModifiedCount > 0;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, @$"Error marking PushContact deleted (_id: {pushContactId})");
+                return false;
+            }
         }
 
     }
