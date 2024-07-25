99dd45097fb9a1c66cc8726cf77e3fd5956f44fb
leoslopez <llopez@makingsense.com>
2024-07-25 17:45:03 -0300

    feat: add support to encrypt/decrypt using Base64Url

--- Doppler.PushContact.Transversal/EncryptionHelper.cs
+++ Doppler.PushContact.Transversal/EncryptionHelper.cs
@@ -15,7 +15,7 @@ namespace Doppler.PushContact.Transversal
             iv = encryptionIV;
         }
 
-        public static string Encrypt(string plainText)
+        public static string Encrypt(string plainText, bool useBase64Url = false)
         {
             using (Aes aesAlg = Aes.Create())
             {
@@ -32,12 +32,13 @@ namespace Doppler.PushContact.Transversal
                         swEncrypt.Write(plainText);
                     }
 
-                    return Convert.ToBase64String(msEncrypt.ToArray());
+                    byte[] encryptedBytes = msEncrypt.ToArray();
+                    return useBase64Url ? Base64UrlEncode(encryptedBytes) : Convert.ToBase64String(encryptedBytes);
                 }
             }
         }
 
-        public static string Decrypt(string cipherText)
+        public static string Decrypt(string cipherText, bool useBase64Url = false)
         {
             using (Aes aesAlg = Aes.Create())
             {
@@ -46,13 +47,38 @@ namespace Doppler.PushContact.Transversal
 
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                byte[] cipherBytes = useBase64Url ? Base64UrlDecode(cipherText) : Convert.FromBase64String(cipherText);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherBytes))
                 using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                 using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                 {
                     return srDecrypt.ReadToEnd();
                 }
             }
+        }
+
+        private static string Base64UrlEncode(byte[] input)
+        {
+            return Convert.ToBase64String(input)
+                .TrimEnd('=')
+                .Replace('+', '-')
+                .Replace('/', '_');
+        }
+
+        private static byte[] Base64UrlDecode(string input)
+        {
+            string output = input
+                .Replace('-', '+')
+                .Replace('_', '/');
+
+            switch (output.Length % 4)
+            {
+                case 2: output += "=="; break;
+                case 3: output += "="; break;
+            }
+
+            return Convert.FromBase64String(output);
         }
     }
 }
