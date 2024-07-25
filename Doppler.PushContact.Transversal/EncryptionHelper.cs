f65b6e152c71f1c80eacc14157afcd7886c9c761
leoslopez <llopez@makingsense.com>
2024-07-25 15:14:11 -0300

    feat: add EncryptionHelper and EncryptionSettings

---
+++ Doppler.PushContact.Transversal/EncryptionHelper.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Doppler.PushContact.Transversal
+{
+    public static class EncryptionHelper
+    {
+        private static string key;
+        private static string iv;
+
+        public static void Initialize(string encryptionKey, string encryptionIV)
+        {
+            key = encryptionKey;
+            iv = encryptionIV;
+        }
+
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(key);
+                aesAlg.IV = Convert.FromBase64String(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
