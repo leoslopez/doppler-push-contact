46cde1b55aa10e06b8ffd22e96d90855bd995e5b
leoslopez <llopez@makingsense.com>
2024-07-29 17:33:44 -0300

    feat: add changes into WebPushEventRepository
    add IsWebPushEventRegistered and InsertAsync methods

--- Doppler.PushContact/Repositories/Interfaces/IWebPushEventRepository.cs
+++ Doppler.PushContact/Repositories/Interfaces/IWebPushEventRepository.cs
@@ -1,5 +1,8 @@
 using Doppler.PushContact.DTOs;
+using Doppler.PushContact.Models.Entities;
+using Doppler.PushContact.Models.Enums;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Doppler.PushContact.Repositories.Interfaces
@@ -7,5 +10,7 @@ namespace Doppler.PushContact.Repositories.Interfaces
     public interface IWebPushEventRepository
     {
         Task<WebPushEventSummarizationDTO> GetWebPushEventSummarization(Guid messageId);
+        Task<bool> InsertAsync(WebPushEvent webPushEvent, CancellationToken cancellationToken);
+        Task<bool> IsWebPushEventRegistered(string pushContactId, Guid messageId, WebPushEventType type);
     }
 }
